0736903112367ca3721cf32809bed36964b980b0
vvihorev <vvihorevv@gmail.com>
2022-04-14 04:26:47 +0300

    Expanded the first FT in commentaries, now using unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'install' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        # To-Do app homepage exists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Tests finished!')
+        
+        # user inputs item into a text field
+        # new element appears on the page
+        # user performs another input
+        # new element appears on the page
+        # a text explaining that a unique URL has been created appears on the page
+        # user visits the generated url, the list is in place
+
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
