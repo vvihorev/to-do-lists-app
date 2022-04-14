a8dd7f6b8b6420d3a387b3ac1c848165159ea18a
vvihorev <vvihorevv@gmail.com>
2022-04-14 17:14:42 +0300

    FT now check the ability to add items to a list

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -15,15 +17,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # To-Do app homepage exists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Tests finished!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
-        # user inputs item into a text field
+        # user is given an option to add a to-do item into a text field
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # user types in an item
+        inputbox.send_keys('Buy feathers')
+
+        # user confirms an input, page updates
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # new element appears on the page
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy feathers' for row in rows)
+        )
+
         # user performs another input
-        # new element appears on the page
+        # yet another element appears on the page
         # a text explaining that a unique URL has been created appears on the page
         # user visits the generated url, the list is in place
 
+        self.fail('Tests finished!')
         browser.quit()
 
 
