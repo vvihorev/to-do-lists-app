59e2945a8e4f83518dc306a4325980e27d5cffb1
vvihorev <vvihorevv@gmail.com>
2022-04-18 17:36:48 +0300

    Added URL for new list, changed tests and URLs for two forms

--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
-    # TODO: support more than one list
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/absolutely-unique-list/')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect ('/lists/absolutely-unique-list/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,6 @@ from lists import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home'),
-    path('lists/absolutely-unique-list/', views.view_list, name='view_list')
+    path('lists/new', views.new_list, name='new_list'),
+    path('lists/absolutely-unique-list/', views.view_list, name='view_list'),
 ]
