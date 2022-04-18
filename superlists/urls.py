171bea6c02c76a1ce1bb28b1ea1e6042f56ad992
vvihorev <vvihorevv@gmail.com>
2022-04-18 23:04:33 +0300

    Refactored urls, created separated urls file for lists, all tests pass

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,26 +1,9 @@
-"""superlists URL Configuration
+from django.urls import include, re_path, path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, re_path
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    re_path(r'^$', list_views.home_page, name='home'),
+    path('lists/', include(list_urls)),
 ]
