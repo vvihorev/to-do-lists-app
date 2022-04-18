52466f1282af26384ab9227287246cc346ab0d1d
vvihorev <vvihorevv@gmail.com>
2022-04-18 11:02:40 +0300

    Added wait function, changed find_elements behaviour

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,13 @@
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -14,21 +17,29 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(by=By.ID, value='id_list_table')
+                rows = table.find_elements(by=By.TAG_NAME, value='tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
 
         # To-Do app homepage exists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
         self.assertIn('To-Do', header_text)
         
         # user is given an option to add a to-do item into a text field
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -39,20 +50,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         # user confirms an input, page updates
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # new element appears on the page
-        self.check_for_row_in_list_table('1: Buy feathers')
+        self.wait_for_row_in_list_table('1: Buy feathers')
 
         # user performs another input
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
         inputbox.send_keys('Make a hat from feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # yet another element appears on the page
-        self.check_for_row_in_list_table('1: Buy feathers')
-        self.check_for_row_in_list_table('2: Make a hat from feathers')
+        self.wait_for_row_in_list_table('1: Buy feathers')
+        self.wait_for_row_in_list_table('2: Make a hat from feathers')
 
         # a text explaining that a unique URL has been created appears on the page
         # user visits the generated url, the list is in place
